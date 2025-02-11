e5dae83d44b885f7328c5b2d336f94b22b3766ad
Àlex Marlés <alexmarles@users.noreply.github.com>
2025-02-11 15:48:12 +0100

    Add Google Analytics (#2)
    * Update pnpm version
    
    * Add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alexmarles.com',
+    integrations: [
+        partytown({
+            config: {
+                forward: ['dataLayer.push'],
+            },
+        }),
+    ],
 });
